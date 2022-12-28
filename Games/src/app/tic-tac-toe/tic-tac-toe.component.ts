c363ee8e50ecd7c713cb9d5fb78c997c2b33a75b
Marcell Tanure <mtanure@waldale.com>
2022-12-28 18:28:49 -0400

    winCKeck() to be fixed

---
+++ Games/src/app/tic-tac-toe/tic-tac-toe.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tic-tac-toe',
+  templateUrl: './tic-tac-toe.component.html',
+  styleUrls: ['./tic-tac-toe.component.scss']
+})
+export class TicTacToeComponent implements OnInit {
+  gameState: string[] = [
+    '', '', '',
+    '', '', '',
+    '', '', ''
+  ]
+
+  xTurn: boolean = true;
+  gameOver: boolean = false;
+
+  combinations: number[][] = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9],
+    [1, 5, 9],
+    [3, 5, 7]
+  ]
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onInput(index: number) {
+    this.gameState[index] = this.xTurn ? 'x' : 'o';
+    this.winCheck();
+    this.xTurn = !this.xTurn;
+  }
+
+  winCheck() {
+    for (let combination of this.combinations) {
+      let a = combination[0];
+      let b = combination[1];
+      let c = combination[2];
+
+      if (this.gameState[a] &&
+          this.gameState[a] === this.gameState[b] &&
+          this.gameState[b] === this.gameState[c]) {
+            this.gameOver = true;
+      }
+    }
+  }
+}
